75c9975150f05f194e6881b74862f919f655d501
Fabio Fortkamp <fabio@fabiofortkamp.com>
2015-11-19 14:14:26 -0200

    Add prm-project

---
+++ prm-data-win/nb/start.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This script will run when STARTING the project "nb"
+# Here you might want to cd into your project directory, activate virtualenvs, etc.
+
+# The currently active project is available via $PRM_ACTIVE_PROJECT
+# Command line arguments can be used, $3 would be the first argument after your project name.
+
+cd $HOME/code/magref-notebooks
+
+start cmd
