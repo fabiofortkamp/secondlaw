bd6e3a19b6ac4fb92122260100b54a00a44c9366
Fabio Fortkamp <fabio@fabiofortkamp.com>
2015-11-19 14:26:26 -0200

    Added an ALTERNATE_EDITOR env variable, and organized the OS specific code

--- secondlaw.sh
+++ secondlaw.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+
+
 #Aliases
 alias gst='git status'
 alias ga='git add'
@@ -8,7 +10,17 @@ alias gcmsg='git commit -m'
 
 alias emacs='emacsclient -n'
 
+
 EDITOR='emacsclient -n'
+ALTERNATE_EDITOR='atom'
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+    PRM_DIR="$SECONDLAW/prm-data-osx"
+elif [[ "$OSTYPE" == "msys" ]]; then
+        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
+    PRM_DIR="$SECONDLAW/prm-data-win"
+fi
 
 # git prompt
 GIT_PROMPT=$SECONDLAW/bash-git-prompt
@@ -26,12 +38,6 @@ GIT_PROMPT_THEME=Solarized
 # PRM
 PRM_INSTALLATION_DIR=$SECONDLAW/prm
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-        # Mac OSX
-    PRM_DIR="$SECONDLAW/prm-data-osx"
-elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-    PRM_DIR="$SECONDLAW/prm-data-win"
-fi
+
 
 alias prm=". $PRM_INSTALLATION_DIR/prm.sh"
