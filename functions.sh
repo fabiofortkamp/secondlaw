ccae7a03fe76090588acbe63823ff20df88beb89
Fábio Fortkamp <fabio@fabiofortkamp.com>
2019-11-01 09:03:15 -0300

    Remove non-working close function for Emacs

--- functions.sh
+++ functions.sh
@@ -1,10 +1,3 @@
-# function to close the buffer corresponding to a given file in emacs
-# cf = close file
-function cf() {
-     emacs -e  "(if (get-buffer \"$1\") (kill-buffer \"$1\"))"
-     }
-
-
 # stolen from https://github.com/robbyrussell/oh-my-zsh/blob/master/lib/functions.zsh
 function take() {
   mkdir -p $1
